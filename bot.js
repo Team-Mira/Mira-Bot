8eaf0cd3e20daef8da87d6330c0eb86f5ec8ef16
Robert Brown <robert.t.brown352@gmail.com>
2022-03-16 22:54:55 -0400

    Added routes to communicate with the app being hosted on heroku

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const axios = require('axios');
 const { Client, Intents } = require('discord.js');
 const intents = new Intents(32767);
 const client = new Client({ intents });
@@ -8,7 +9,69 @@ client.on('ready', () => {
 });
 
 client.on("messageCreate", msg => {
-  console.log(msg)
+  const { guildId, channelId, author, id, content } = msg
+  const isReply = msg.type === 'REPLY' ? true : false
+  const repliedUser = msg.mentions.repliedUser === null ? null : msg.mentions.repliedUser.id
+
+  if(content !== '' && !(author.bot)){
+    const user = {
+      id: author.id,
+      name: author.username
+    }
+
+    const message = {
+      id,
+      content,
+      isReply,
+      repliedUser
+    }
+
+    const server = {
+      id: guildId
+    }
+
+    const channel = {
+      id: channelId
+    }
+
+    const body = {
+      user,
+      message,
+      server,
+      channel,
+    }
+
+    // axios.post('http://localhost:8080/api/message/add', body)
+    axios.post('https://mira-api-cs.herokuapp.com/api/message/add', body)
+  }
 })
+
+client.on('messageDelete', msg => {
+  const { id } = msg
+  axios.delete('https://mira-api-cs.herokuapp.com/api/message/delete', { data: { id }})
+})
+
+client.on('messageUpdate', msg => {
+  const { id } = msg
+  const { content } = msg.reactions.message
+
+  axios.put('https://mira-api-cs.herokuapp.com/api/message/update', { id, content })
+})
+
+
+client.on('messageReactionAdd', reaction => {
+  const message = {
+    id: reaction.message.id
+  }
+  axios.put('https://mira-api-cs.herokuapp.com/api/message/reaction/add', { message })
+})
+
+client.on('messageReactionRemove', reaction => {
+  const message = {
+    id: reaction.message.id
+  }
+  axios.put('https://mira-api-cs.herokuapp.com/api/message/reaction/remove', { message })
+})
+
 
 client.login(process.env.BOT_TOKEN)
